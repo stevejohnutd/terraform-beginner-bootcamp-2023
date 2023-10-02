4a7955aa32aea7b45884e101511a25a12e755257
Steve John <stevejohnutd@gmail.com>
2023-10-02 05:17:17 +0000

    fixing

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -28,7 +28,7 @@ resource "aws_s3_object" "index_html" {
   key    = "index.html"
   source = "${var.index_html_filepath}"
   content_type = "text/html"
-  etag = filemd5("${path.root}${var.index_html_filepath}")
+  etag = filemd5("${var.index_html_filepath}")
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -37,7 +37,7 @@ resource "aws_s3_object" "error_html" {
   key    = "error.html"
   source = "${var.error_html_filepath}"
 
-  etag = filemd5("${path.root}${var.error_html_filepath}")
+  etag = filemd5("${var.error_html_filepath}")
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
