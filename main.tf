7adbb8b2333f73029b87daf86bb85f083199de02
Steve John <98290599+stevejohnutd@users.noreply.github.com>
2023-10-08 00:25:19 -0500

    42 terratowns provider (#47)
    * #47 wip setting up the skeleton for creating a terraform custom provider
    
    * #42 Should be able to create run terraform provider block without errors
    
    * #42 setup skeleton for resource
    
    * Updating server.rb file
    
    * #42 Finish implementation for terraform provider

--- main.tf
+++ main.tf
@@ -26,5 +26,17 @@ terraform {
 provider "terratowns" {
   endpoint = "http://localhost:4567/api"
   user_uuid="9a92407d-a511-40ef-a6bf-44e615cf0e03" 
-  token="8f470175-0488-4751-af5d-c81132cbac41"
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+resource "terratowns_home" "home" {
+  name = "Dune (2021)"
+  description = <<DESCRIPTION
+A noble family becomes embroiled in a war for control over the galaxy's
+most valuable asset while its heir becomes troubled by visions of a dark future. Director - Denis Villeneuve
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "video-valley"
+  content_version = 1
 }
