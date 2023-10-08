98a90e80530a3c7d33ff8ef898375a416c34a8ef
Steve John <stevejohnutd@gmail.com>
2023-10-08 06:46:03 +0000

    #48 Test our terra home on terratowns test town missingo

--- main.tf
+++ main.tf
@@ -13,20 +13,20 @@ terraform {
   # }
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid=var.user_uuid
-#   bucket_name=var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid=var.teacherseat_user_uuid
+  #bucket_name=var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="9a92407d-a511-40ef-a6bf-44e615cf0e03" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "https://terratowns.cloud/api"
+  user_uuid=var.teacherseat_user_uuid
+  token=var.terratowns_access_token
 }
 
 resource "terratowns_home" "home" {
@@ -35,8 +35,8 @@ resource "terratowns_home" "home" {
 A noble family becomes embroiled in a war for control over the galaxy's
 most valuable asset while its heir becomes troubled by visions of a dark future. Director - Denis Villeneuve
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "video-valley"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
-variable "user_uuid" {
+variable "terratowns_access_token" {
  type = string
 }
-
+variable "teacherseat_user_uuid" {
+ type = string
+}
+variable "terratowns_endpoint" {
+ type = string
+}
 variable "bucket_name" {
  type = string
 }
